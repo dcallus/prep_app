18252c62a436f65102a14b780297c58af2da69ca
Daniel Callus <danielcallus@gmail.com>
2021-06-30 15:57:52 +0100

    added setMeal and CustomMeal objects

--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/CustomMeal.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/CustomMeal.java
@@ -1,4 +1,37 @@
 package com.meal_prep.MealPrepApp.models.food;
 
+import com.meal_prep.MealPrepApp.components.enums.AllergenType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class CustomMeal extends FoodItem{
+
+    private ArrayList<FoodItem> customMealItems;
+
+    public CustomMeal(String name, Integer protein, Integer carbs, Integer fats, Integer calories, Integer totalWeight,
+                      ArrayList<String> ingredients, HashMap<AllergenType, Boolean> allergens, Double price,
+                      String imageUrl, ArrayList<FoodItem> customMealItems) {
+        super(name, protein, carbs, fats, calories, totalWeight, ingredients, allergens, price, imageUrl);
+        this.customMealItems = customMealItems;
+        // potentially use composition to replace mac/cal/fat etc properties by using an addition method
+        // if this doesn't work - potentially, new class of food with same properties as FoodItem (and abstract
+        // FoodItem class).
+    }
+
+    public CustomMeal(ArrayList<FoodItem> customMealItems) {
+        this.customMealItems = customMealItems;
+    }
+
+    public CustomMeal(){
+
+    }
+
+    public ArrayList<FoodItem> getCustomMealItems() {
+        return customMealItems;
+    }
+
+    public void setCustomMealItems(ArrayList<FoodItem> customMealItems) {
+        this.customMealItems = customMealItems;
+    }
 }
--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/SetMeal.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/SetMeal.java
@@ -1,4 +1,52 @@
 package com.meal_prep.MealPrepApp.models.food;
 
+import com.meal_prep.MealPrepApp.components.enums.AllergenType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SetMeal extends FoodItem{
+
+    private String mainProtein;
+    private String description;
+    private String category; //breakfast/lunch/dinner/snack/muscle-gain/weight-loss
+
+    public SetMeal(String name, Integer protein, Integer carbs, Integer fats, Integer calories, Integer totalWeight,
+                   ArrayList<String> ingredients, HashMap<AllergenType, Boolean> allergens, Double price,
+                   String imageUrl, String mainProtein, String description, String category) {
+        super(name, protein, carbs, fats, calories, totalWeight, ingredients, allergens, price, imageUrl);
+        this.mainProtein = mainProtein;
+        this.description = description;
+        this.category = category;
+    }
+
+    public SetMeal(String mainProtein, String description, String category) {
+        this.mainProtein = mainProtein;
+        this.description = description;
+        this.category = category;
+    }
+
+    public String getMainProtein() {
+        return mainProtein;
+    }
+
+    public void setMainProtein(String mainProtein) {
+        this.mainProtein = mainProtein;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
