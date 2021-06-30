b64d517836028b0512453d7b704619768c43c7ba
Daniel Callus <danielcallus@gmail.com>
2021-06-30 15:23:49 +0100

    added client/server folders

---
+++ MealPrepApp/Server/src/test/java/com/meal_prep/MealPrepApp/MealPrepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meal_prep.MealPrepApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPrepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
