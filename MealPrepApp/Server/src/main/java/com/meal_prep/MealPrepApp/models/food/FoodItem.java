d6a68c50ea9f5a239a65efcff195af466ca89075
Daniel Callus <danielcallus@gmail.com>
2021-06-30 15:50:15 +0100

    basic foodItem done

--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/FoodItem.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/models/food/FoodItem.java
@@ -9,15 +9,118 @@ public class FoodItem {
 
     private String id;
     private String name;
-    private int protein;
-    private int carbs;
-    private int fats;
-    private int calories;
-    private int totalWeight;
+    private Integer protein;
+    private Integer carbs;
+    private Integer fats;
+    private Integer calories;
+    private Integer totalWeight;
     private ArrayList<String> ingredients;
     private HashMap<AllergenType, Boolean> allergens;
-    private double price;
+    private Double price;
     private String imageUrl;
 
 
+    public FoodItem(String name, Integer protein, Integer carbs, Integer fats, Integer calories, Integer totalWeight, ArrayList<String> ingredients, HashMap<AllergenType, Boolean> allergens, Double price, String imageUrl) {
+        this.name = name;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fats = fats;
+        this.calories = calories;
+        this.totalWeight = totalWeight;
+        this.ingredients = ingredients;
+        this.allergens = allergens;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public FoodItem() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getProtein() {
+        return protein;
+    }
+
+    public void setProtein(Integer protein) {
+        this.protein = protein;
+    }
+
+    public Integer getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(Integer carbs) {
+        this.carbs = carbs;
+    }
+
+    public Integer getFats() {
+        return fats;
+    }
+
+    public void setFats(Integer fats) {
+        this.fats = fats;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public Integer getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(Integer totalWeight) {
+        this.totalWeight = totalWeight;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public HashMap<AllergenType, Boolean> getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(HashMap<AllergenType, Boolean> allergens) {
+        this.allergens = allergens;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
