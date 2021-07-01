1d9ff2db60decc06376beaf8bbd9aac14a0940ae
Craig <craigwilsonmusic@gmail.com>
2021-07-01 16:41:56 +0100

    food controller, set up first three routes

--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/controllers/FoodController.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/controllers/FoodController.java
@@ -3,11 +3,16 @@ package com.meal_prep.MealPrepApp.controllers;
 import com.meal_prep.MealPrepApp.models.food.FoodItem;
 import com.meal_prep.MealPrepApp.repositories.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -17,10 +22,26 @@ public class FoodController {
     @Autowired
     FoodRepository foodRepository;
 
-    @GetMapping(value = "/food")
-    public ResponseEntity getAllFood(){
-        return new ResponseEntity(foodRepository.findAll(), HttpStatus.OK);
+    // GET FoodItem
+    @RequestMapping(method=RequestMethod.GET, value = "/food")
+    public Iterable<FoodItem> getAllFood(){
+        return foodRepository.findAll();
     }
 
+    // POST FoodItem
+    @RequestMapping(method=RequestMethod.POST, value= "/food")
+    public FoodItem save(@RequestBody FoodItem foodItem) {
+        foodRepository.save(foodItem);
+        return foodItem;
+    }
 
+    @RequestMapping(method=RequestMethod.GET, value = "/food/{id}")
+    public Optional<FoodItem> getFoodById(@PathVariable String id){
+        return foodRepository.findById(id);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/food/{id}")
+    public Optional<FoodItem> show(@PathVariable String id) {
+        return foodRepository.findById(id);
+    }
 }
