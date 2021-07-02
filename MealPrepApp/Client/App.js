c971a2f3f32ecb7a3a59ae5a5a039e5400231b37
Craig <craigwilsonmusic@gmail.com>
2021-07-02 12:40:50 +0100

    committing

---
+++ MealPrepApp/Client/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MealAppService from './services/MealAppService';
+
+export default function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    MealAppService.getFoodItem()
+        .then(items => setItems(items))
+  }, []);
+
+  useEffect(() => {
+    console.log("list of items: ", items);
+  }, [items])
+
+  return (
+    items ?
+    <View style={styles.container}>
+      <Text>React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+    :
+    <View></View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ MealPrepApp/Client/services/MealAppService.js
@@ -0,0 +1,25 @@
+const baseURL = 'http://localhost:8080/'
+
+const MealAppService = {
+  getFoodItem() {
+    return fetch(baseURL + "food/")
+    .then(res => res.json())
+  },
+
+  postFoodItem(payload) {
+    return fetch(baseURL + "food/", {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { 'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+  },
+
+  deleteCard(id) {
+    return fetch(baseURL + "food/" + id, {
+      method: 'DELETE'
+    })
+  }
+}
+
+export default MealAppService;
