b09f6105b73b6389e73f7a4593962f95b0c8282e
Daniel Callus <danielcallus@gmail.com>
2021-07-03 19:41:32 +0100

    remember to change branch dan

--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/controllers/ShopController.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/controllers/ShopController.java
@@ -1,4 +1,25 @@
 package com.meal_prep.MealPrepApp.controllers;
 
+import com.meal_prep.MealPrepApp.models.Shop;
+import com.meal_prep.MealPrepApp.repositories.ShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ShopController {
+
+    @Autowired
+    ShopRepository shopRepository;
+
+    // GET
+    @RequestMapping(method= RequestMethod.GET, value = "/shop")
+    public Iterable<Shop> getAllShops(){
+        return shopRepository.findAll();
+    }
+
+
+
+
 }
--- MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/repositories/ShopRepository.java
+++ MealPrepApp/Server/src/main/java/com/meal_prep/MealPrepApp/repositories/ShopRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShopRepository extends MongoRepository<Shop, String> {
+
 }
