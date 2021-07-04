0b0ad86738eb3b9d84a72940b980c652e250325a
Craig <craigwilsonmusic@gmail.com>
2021-07-04 15:01:35 +0100

    created theme.js file, and icons/fonts imported

---
+++ MealPrepApp/Client/app/constants/icons.js
@@ -0,0 +1,9 @@
+const drink = require("../assets/icons/drink.png")
+const location = require("../assets/icons/location.png")
+const noodle = require("../assets/icons/noodle.png")
+
+export default(
+    drink,
+    location,
+    noodle
+)
---
+++ MealPrepApp/Client/app/constants/images.js
@@ -0,0 +1,7 @@
+const logo2 = require("../assets/images/logo2.png")
+const background = require("../assets/images/background.jpg")
+
+export default (
+    logo2,
+    background
+)
